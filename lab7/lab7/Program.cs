3130718464ba05de88b1bd4010e74b9867a439f8
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-03 12:56:22 +0300

    prog

--- lab7/lab7/Program.cs
+++ lab7/lab7/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace lab7;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var queue = new Queue();
+
+        var emitter = new EventEmitter(emitterId: 1, intervalMs: 1000, queue);
+        var handler1 = new EventHandler(handlerId: 1, processingTimeMs: 1500, queue);
+        var handler2 = new EventHandler(handlerId: 2, processingTimeMs: 1200, queue);
+
+        var cts = new CancellationTokenSource();
+
+        var emitterTask = emitter.StartAsync(cts.Token);
+        var handlerTask1 = handler1.StartAsync(cts.Token);
+        var handlerTask2 = handler2.StartAsync(cts.Token);
+        Console.WriteLine("Нажмите любую клавишу для остановки...");
+        Console.ReadKey();
+        cts.Cancel();
+
+        await Task.WhenAll(emitterTask, handlerTask1, handlerTask2); 
+        Console.WriteLine("Система остановлена.");
+    }
+}
--- lab7/lab7/obj/Debug/net8.0/lab7.AssemblyInfo.cs
+++ lab7/lab7/obj/Debug/net8.0/lab7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daaa63f41cbc5c02564d02b89e6ace470df6bac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37416df38e6466c22b5f10f125a8b21a7419706")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
