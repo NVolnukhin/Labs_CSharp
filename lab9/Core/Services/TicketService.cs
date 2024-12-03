f37b21c8f63a4248011dbd3bfea9f0b4b05f2d52
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-04 00:47:08 +0300

    ticketRepo

--- lab9/Core/Services/TicketService.cs
+++ lab9/Core/Services/TicketService.cs
@@ -1,6 +1,21 @@
+using DatabaseContext.Interfaces;
+using DatabaseModel;
+
 namespace Core.Services;
 
 public class TicketService
 {
+    private readonly ITicketRepository _ticketRepository;
+
+    public TicketService(ITicketRepository ticketRepository)
+    {
+        _ticketRepository = ticketRepository;
+    }
     
+    public async Task Add(Guid exhibitionId, Exhibition exhibition, Guid visitorId, Visitor visitor)
+    {
+        var ticket = Ticket.Create(exhibitionId, exhibition, visitorId, visitor);
+
+        await _ticketRepository.Add(ticket);
+    }
 }
--- lab9/DatabaseModel/Entities/Ticket.cs
+++ lab9/DatabaseModel/Entities/Ticket.cs
@@ -2,16 +2,23 @@ namespace DatabaseModel;
 
 public class Ticket
 {
-    private Ticket(Guid id, Exhibition exhibition, string name, DateTime date)
+    private Ticket(Guid id, Guid exhibitionId, Exhibition exhibition, Guid visitorId, Visitor visitor)
     {
         Id = id;
-        Name = name;
-        Date = date;
+        ExhibitionId = exhibitionId;
+        Exhibition = exhibition;
+        VisitorId = visitorId;
+        Visitor = visitor;
     }
     
     public Guid Id { get; init; } // Primary Key
     public Guid ExhibitionId { get; init; } // FK на выставку
-    public Exhibition Exhibition { get; set; } = null!;
+    public Exhibition Exhibition { get; set; }
     public Guid VisitorId { get; init; } // FK на посетителя
-    public Visitor Visitor { get; set; } = null!;
+    public Visitor Visitor { get; set; }
+    
+    public static Ticket Create(Guid exhibitionId, Exhibition exhibition, Guid visitorId, Visitor visitor)
+    {
+        return new Ticket(Guid.NewGuid(), exhibitionId, exhibition, visitorId, visitor);
+    } 
 }
