cc3cf760e9b063013f9d48ff403668d67e52e57d
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-03 23:55:11 +0300

    visitorRepo

--- lab9/DatabaseContext/Repositories/VisitorRepository.cs
+++ lab9/DatabaseContext/Repositories/VisitorRepository.cs
@@ -1,6 +1,19 @@
+using DatabaseModel;
+
 namespace DatabaseContext.Repositories;
 
 public class VisitorRepository
 {
+    private readonly AppDbContext _appDbContext;
     
+    public VisitorRepository(AppDbContext dbContext)
+    {
+        _appDbContext = dbContext;
+    }
+    
+    public async Task Add(Visitor visitor)
+    {
+        await _appDbContext.Visitors.AddAsync(visitor);
+        await _appDbContext.SaveChangesAsync();
+    }
 }
