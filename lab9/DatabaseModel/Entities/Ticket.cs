4d65e1271921486b4454d508b1602b7ef637e35d
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-04 00:41:51 +0300

    exhibitionFacadeUpdate

--- lab9/DatabaseModel/Entities/Ticket.cs
+++ lab9/DatabaseModel/Entities/Ticket.cs
@@ -2,9 +2,16 @@ namespace DatabaseModel;
 
 public class Ticket
 {
+    private Ticket(Guid id, Exhibition exhibition, string name, DateTime date)
+    {
+        Id = id;
+        Name = name;
+        Date = date;
+    }
+    
     public Guid Id { get; init; } // Primary Key
-    public int ExhibitionId { get; init; } // FK на выставку
+    public Guid ExhibitionId { get; init; } // FK на выставку
     public Exhibition Exhibition { get; set; } = null!;
-    public int VisitorId { get; init; } // FK на посетителя
+    public Guid VisitorId { get; init; } // FK на посетителя
     public Visitor Visitor { get; set; } = null!;
 }
