5dbf19bdab7d21131259f70ba6b7a2fed08d1d2a
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-03 13:50:20 +0300

    configurationChanged

--- lab7/lab7/Configuration.cs
+++ lab7/lab7/Configuration.cs
@@ -2,10 +2,12 @@ namespace lab7;
 
 public class Configuration
 {
-    //интервал генерации событий (стекла)
+    //интервал генерации событий (генерация поставки)
     public int EmitterInterval { get; set; }
-    //интервал обработки событий (выдувки стекла)
+    //интервал обработки событий (сборки поставки)
     public int[] ProcessorDelays { get; set; }
+    //интервал обработки событий 2 (доставка стекла получателю)
+    public int[] RecieverDelays { get; set; }
     //продолжительность симуляции
     public int SimulationDuration { get; set; }
 }
