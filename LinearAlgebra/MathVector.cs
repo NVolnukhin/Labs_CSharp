399218f4d99766994e8b9c04e4b711cff4532f38
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-03 10:26:46 +0300

    lab5

---
+++ LinearAlgebra/MathVector.cs
@@ -0,0 +1,151 @@
+﻿namespace LinearAlgebra;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MathVector : IMathVector
+{
+    private double[] _components;
+
+    public MathVector(params double[] _components)
+    {
+        if (_components.Length == 0)
+        {
+            throw new ArgumentException("Вектор должен содержать хотя бы одно значение.");
+        }
+        this._components = new double[_components.Length];
+        Array.Copy(_components, this._components, _components.Length);
+    }
+
+    public int Dimensions => _components.Length;
+
+    public double this[int i]
+    {
+        get 
+        {
+            if (i < 0 || i >= this.Dimensions)
+            {
+                throw new ArgumentException("Неверный индекс.");
+            }
+            else 
+            {
+                return _components[i];
+            }
+        }
+        set => _components[i] = value;
+    }
+
+    public double Length => Math.Sqrt(_components.Sum(c => c * c));
+
+    public IMathVector SumNumber(double number)
+    {
+        return new MathVector(_components.Select(c => c + number).ToArray());
+    }
+
+    public IMathVector MultiplyNumber(double number)
+    {
+        return new MathVector(_components.Select(c => c * number).ToArray());
+    }
+
+    public IMathVector Sum(IMathVector vector)
+    {
+        if (Dimensions != vector.Dimensions)
+        {
+            throw new ArgumentException("Размерности векторов не совпадают.");
+        }
+        return new MathVector(_components.Select((c, i) => c + vector[i]).ToArray());
+    }
+
+    public IMathVector Multiply(IMathVector vector)
+    {
+        if (Dimensions != vector.Dimensions)
+        {
+            throw new ArgumentException("Размерности векторов не совпадают.");
+        }
+        return new MathVector(_components.Select((c, i) => c * vector[i]).ToArray());
+    }
+
+    public double ScalarMultiply(IMathVector vector)
+    {
+        if (Dimensions != vector.Dimensions)
+        {
+            throw new ArgumentException("Размерности векторов не совпадают.");
+        }
+        return _components.Select((c, i) => c * vector[i]).Sum();
+    }
+
+    public double CalcDistance(IMathVector vector)
+    {
+        if (Dimensions != vector.Dimensions)
+        {
+            throw new ArgumentException("Размерности векторов не совпадают.");
+        }
+        return Math.Sqrt(_components.Select((c, i) => Math.Pow(c - vector[i], 2)).Sum());
+    }
+
+    public IEnumerator<double> GetEnumerator()
+    {
+        return ((IEnumerable<double>)_components).GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override string ToString()
+    {
+        return $"({string.Join(", ", _components)})";
+    }
+
+    public static IMathVector operator +(MathVector vector, double number)
+    {
+        return vector.SumNumber(number);
+    }
+
+    public static IMathVector operator -(MathVector vector, double number)
+    {
+        return vector.SumNumber(-number);
+    }
+
+    public static IMathVector operator *(MathVector vector, double number)
+    {
+        return vector.MultiplyNumber(number);
+    }
+
+    public static IMathVector operator +(MathVector vector1, IMathVector vector2)
+    {
+        try
+        {
+            return vector1.Sum(vector2);
+        }
+        catch (ArgumentException)
+        {
+            throw;
+        }
+    }
+
+    public static IMathVector operator -(MathVector vector1, IMathVector vector2)
+    {
+        try
+        {
+            return vector1.Sum(vector2.MultiplyNumber(-1));
+        }
+        catch (ArgumentException)
+        {
+            throw;
+        }
+    }
+
+    public static IMathVector operator *(MathVector vector1, IMathVector vector2)
+    {
+        try
+        {
+            return vector1.Multiply(vector2);
+        }
+        catch (ArgumentException)
+        {
+            throw;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using LinearAlgebra;
+
+namespace lab5;
+
+class Program
+{
+    static void Main()
+    {
+        MathVector v1 = new MathVector(3, 8);
+        MathVector v2 = new MathVector(1, 2);
+        MathVector v3 = new MathVector(1, 2, 3);      
+        MathVector infVec = new MathVector(double.PositiveInfinity, double.NegativeInfinity);      
+
+        Console.WriteLine("\nВектор 1: " + v1);
+        Console.WriteLine("Вектор 2: " + v2);
+        Console.WriteLine("Вектор 3: " + v3);
+        Console.WriteLine("Вектор бесконечности: " + infVec);
+
+        IMathVector v1Mod = v1 + 70;
+        Console.WriteLine("\n" + v1 + " + 70 = " + v1Mod);
+
+        IMathVector v2Mod = v2 * 7;
+        Console.WriteLine("\n" + v2 + " * 7 = " + v2Mod);
+
+        Console.WriteLine("\n Попытка сложить вектора 1 и 3...");
+        try
+        {
+            IMathVector sum = v1 + v3;
+            Console.WriteLine("Сумма векторов 1 и 3: " + sum);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        Console.WriteLine("\n Попытка перемножить вектора 1 и 2...");
+        try
+        {
+            IMathVector multiply = v1 * v2;
+            Console.WriteLine("Произведение векторов 1 и 2: " + multiply);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        Console.WriteLine("\n Попытка найти евклидово расстояние между векторами 1 и 2...");
+        try
+        {
+            double distance = v1.CalcDistance(v2);
+            Console.WriteLine("Евклидово расстояние между векторами 1 и 2: " + distance);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        
+        Console.WriteLine("\n Попытка найти скалярное произведение векторов 1 и 2...");
+        try
+        {
+            double scalar = v1.ScalarMultiply(v2);
+            Console.WriteLine("Скалярное произведение векторов 1 и 2: " + scalar);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        
+        Console.WriteLine("\n 0ой элемент вектора 3: " + v3[0]);
+
+        Console.WriteLine("\n Длина вектора 3: " + v3.Length);
+
+        Console.WriteLine("\nПрисвоим 1ому элементу вектора 3 значение 88, а 10ому - 77");
+        try
+        {
+            v3[1] = 88;
+            v3[10] = 77;
+        }
+        catch (System.IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        Console.WriteLine("Вектор 3 после изменения: " + v3);
+    }
+}
