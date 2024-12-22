0c25882eeddef7fb3b9507b9820233bf95586f37
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-22 22:37:26 +0300

    tests

--- lab8/lab8/obj/Debug/net8.0/lab8.AssemblyInfo.cs
+++ lab8/lab8/obj/Debug/net8.0/lab8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0769b0dc9db02382a2dac38397acdaceb56c6366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4dbb428dd703e8fb99b6ca67936d7220f8cfdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
