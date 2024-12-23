463747e633fc08a2e4c8e3a62e0558f9128981ec
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2024-12-23 16:02:23 +0300

    mathVector

--- lab8/LinearAlgebra/obj/Debug/net8.0/LinearAlgebra.AssemblyInfo.cs
+++ lab8/LinearAlgebra/obj/Debug/net8.0/LinearAlgebra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinearAlgebra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c969524b75c99586a4071585227b95996fb199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ece627c78910f094871d90396b70f745a3ac7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinearAlgebra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinearAlgebra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
